2795d54682036f94f1424c656854567ade757c43
CPL <carlos@centropyme.com>
2025-05-12 09:46:50 +0200

    * Creación de proyecto y configuración de acceso a BBDD. * Fijar niveles de loggin para JPA e Hibernate

---
+++ src/main/java/org/cplcursos/jpahibernateejercicioclase/JpaHibernateEjercicioclaseApplication.java
@@ -0,0 +1,13 @@
+package org.cplcursos.jpahibernateejercicioclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateEjercicioclaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateEjercicioclaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cplcursos/jpahibernateejercicioclase/JpaHibernateEjercicioclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cplcursos.jpahibernateejercicioclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateEjercicioclaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
